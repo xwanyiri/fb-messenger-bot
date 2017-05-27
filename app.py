05eb0ad144e75a65d4e847fe14a10f009fe6cf44
xwanyiri <xwanyiri@users.noreply.github.com>
2017-05-27 19:48:21 +0300

    Update app.py
    use "wanyiri" as ur secret code

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+        if not request.args.get("hub.verify_token") == "wanyiri":
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
